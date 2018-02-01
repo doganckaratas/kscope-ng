939792ffb5576a1d4564682773103bdd4ee0d4c4
Dogan Can Karatas <dogan.karatas@airties.com>
2018-02-01 14:26:31 +0300

    Build script should now functional

--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-qmake -makefile -o "Makefile.qscintilla" "CONFIG += qscintilla" src/editor/qscintilla.pri
-sleep 1;
-qmake -makefile -o "Makefile.kscope" "CONFIG += kscope" kscope-ng.pro
-sleep 1;
-
-make -f Makefile.qscintilla all clean &
-make -f Makefile.kscope all clean &
-
-rm Makefile.qscintilla Makefile.kscope
+qmake -makefile -o "Makefile.qscintilla" "CONFIG += qscintilla" src/editor/qscintilla.pri && qmake -makefile -o "Makefile.kscope" "CONFIG += kscope" kscope-ng.pro && (make -f Makefile.qscintilla all clean &) && (make -f Makefile.kscope all clean &) && rm Makefile.qscintilla Makefile.kscope
