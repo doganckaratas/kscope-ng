10af48e2430e0fb230bf261b1ba9bea639d2c080
Dogan Can Karatas <dogan.karatas@airties.com>
2018-02-01 14:23:01 +0300

    Fixed build script again

--- build.sh
+++ build.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-qmake -makefile -o Makefile.qscintilla "CONFIG += qscintilla" kscope-ng.pro;
-qmake -makefile -o Makefile.kscope "CONFIG += kscope" kscope-ng.pro;
-if [ -e Makefile.qscintilla ]
-then
-	make -f Makefile.qscintilla all clean &
-fi
+qmake -makefile -o "Makefile.qscintilla" "CONFIG += qscintilla" src/editor/qscintilla.pri
+sleep 1;
+qmake -makefile -o "Makefile.kscope" "CONFIG += kscope" kscope-ng.pro
+sleep 1;
 
-if [ -e Makefile.kscope ]
-then
-	make -f Makefile.kscope all clean &
-fi
+make -f Makefile.qscintilla all clean &
+make -f Makefile.kscope all clean &
+
 rm Makefile.qscintilla Makefile.kscope
